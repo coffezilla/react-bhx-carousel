a49083451a675b32b8d59551ea00120140a6da41
coffezilla <66714814+coffezilla@users.noreply.github.com>
2021-09-10 21:03:49 -0300

    working with columns

---
+++ react-app/src/App.tsx
@@ -0,0 +1,123 @@
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<div className="grid-swipe-full grid-col-1">
+				<div className="grid-item grid-item--example">
+					<img
+						src="https://cdn.cloudflare.steamstatic.com/steam/apps/236430/header.jpg"
+						alt="dar"
+					/>
+				</div>
+				<div className="grid-item grid-item--example">
+					<img
+						src="https://cdn.cloudflare.steamstatic.com/steam/apps/236430/header.jpg"
+						alt="dar"
+					/>
+				</div>
+				<div className="grid-item grid-item--example">
+					<img
+						src="https://cdn.cloudflare.steamstatic.com/steam/apps/236430/header.jpg"
+						alt="dar"
+					/>
+				</div>
+			</div>
+
+			<div className="grid-swipe grid-col-1">
+				<div className="grid-item grid-item--example">
+					<img
+						src="https://cdn.cloudflare.steamstatic.com/steam/apps/236430/header.jpg"
+						alt="dar"
+					/>
+				</div>
+				<div className="grid-item grid-item--example">
+					<img
+						src="https://cdn.cloudflare.steamstatic.com/steam/apps/236430/header.jpg"
+						alt="dar"
+					/>
+				</div>
+				<div className="grid-item grid-item--example">
+					<img
+						src="https://cdn.cloudflare.steamstatic.com/steam/apps/236430/header.jpg"
+						alt="dar"
+					/>
+				</div>
+			</div>
+
+			<div className="grid-swipe grid-col-2">
+				<div className="grid-item grid-item--example">
+					<img
+						src="https://cdn.cloudflare.steamstatic.com/steam/apps/236430/header.jpg"
+						alt="dar"
+					/>
+				</div>
+				<div className="grid-item grid-item--example">
+					<img
+						src="https://cdn.cloudflare.steamstatic.com/steam/apps/236430/header.jpg"
+						alt="dar"
+					/>
+				</div>
+				<div className="grid-item grid-item--example">
+					<img
+						src="https://cdn.cloudflare.steamstatic.com/steam/apps/236430/header.jpg"
+						alt="dar"
+					/>
+				</div>
+				<div className="grid-item grid-item--example">
+					<img
+						src="https://cdn.cloudflare.steamstatic.com/steam/apps/236430/header.jpg"
+						alt="dar"
+					/>
+				</div>
+				<div className="grid-item grid-item--example">
+					<img
+						src="https://cdn.cloudflare.steamstatic.com/steam/apps/236430/header.jpg"
+						alt="dar"
+					/>
+				</div>
+			</div>
+
+			<div className="grid-swipe grid-col-3">
+				<div className="grid-item grid-item--example">
+					<img
+						src="https://cdn.cloudflare.steamstatic.com/steam/apps/236430/header.jpg"
+						alt="dar"
+					/>
+				</div>
+				<div className="grid-item grid-item--example">
+					<img
+						src="https://cdn.cloudflare.steamstatic.com/steam/apps/236430/header.jpg"
+						alt="dar"
+					/>
+				</div>
+				<div className="grid-item grid-item--example">
+					<img
+						src="https://cdn.cloudflare.steamstatic.com/steam/apps/236430/header.jpg"
+						alt="dar"
+					/>
+				</div>
+				<div className="grid-item grid-item--example">
+					<img
+						src="https://cdn.cloudflare.steamstatic.com/steam/apps/236430/header.jpg"
+						alt="dar"
+					/>
+				</div>
+				<div className="grid-item grid-item--example">
+					<img
+						src="https://cdn.cloudflare.steamstatic.com/steam/apps/236430/header.jpg"
+						alt="dar"
+					/>
+				</div>
+				<div className="grid-item grid-item--example">
+					<img
+						src="https://cdn.cloudflare.steamstatic.com/steam/apps/236430/header.jpg"
+						alt="dar"
+					/>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default App;
