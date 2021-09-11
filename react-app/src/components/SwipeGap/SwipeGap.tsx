bd1a3e30079311729d2e5336a4bc465f793bf4ca
coffezilla <66714814+coffezilla@users.noreply.github.com>
2021-09-11 13:18:28 -0300

    column working but with no variables

---
+++ react-app/src/components/SwipeGap/SwipeGap.tsx
@@ -0,0 +1,45 @@
+import './SwipeGap.css';
+
+interface IPropsGap {
+	children: any;
+	fullscreen?: boolean;
+	column?: number;
+	gap?: number;
+	responsiveLimit?: number;
+	customClass?: string;
+}
+
+interface IPropsItem {
+	children: any;
+	customClass?: string;
+}
+
+const SwipeGap = ({
+	children,
+	fullscreen = false,
+	column = 1,
+	gap = 10,
+	responsiveLimit = 700,
+	customClass,
+}: IPropsGap) => {
+	console.log(responsiveLimit, gap);
+	return (
+		<div
+			className={`${fullscreen ? 'grid-swipe--full' : 'grid-swipe--gap'}  ${
+				customClass && customClass
+			} grid-col-${column}
+      grid-swipe
+      grid-swipe--gap-1
+      grid-swipe--margin-1
+      grid-swipe-gap-1`}
+		>
+			{children}
+		</div>
+	);
+};
+
+export const SwipeItem = ({ children, customClass }: IPropsItem) => {
+	return <div className={`grid-item  ${customClass && customClass}`}>{children}</div>;
+};
+
+export default SwipeGap;
